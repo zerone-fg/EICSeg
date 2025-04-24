7df1125a5e7a460040d6ddc6592c3589b83055e1
zerone-fg <52369263+zerone-fg@users.noreply.github.com>
2025-04-24 12:08:08 +0800

    Update spine.py

--- eval/spine.py
+++ eval/spine.py
@@ -2,11 +2,7 @@ import torch.nn.functional as F
 import torch
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 import os
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import sys
-sys.path.append('/newdata3/xsa/UniverSeg-main')
-sys.path.append('/newdata3/xsa/ICUSeg/mambamodel')
 
 from universeg import universeg
 import numpy as np
@@ -174,33 +170,11 @@ def inference_multi_our(model, image, label_onehot, support_images, support_labe
         support_images,
         ref_masks, mode='test'
     )
-    
-    # torch.cuda.synchronize()
-    # time_end = time.time()
-    # time_sum = time_end - time_start
-    # print("time_our:" , time_sum)
-    # global timecnt
-    # timecnt += time_sum
 
     soft_pred = torch.sigmoid(logits)
-    # soft_pred = logits
     soft_pred_onehot = soft_pred[:, :n_labels, :, :].transpose(0, 1)  ###### (1, 10, 448, 448)
     hard_pred = torch.tensor(soft_pred_onehot > 0.5, dtype=torch.uint8)
     print(torch.sum(hard_pred))
-
-    save_mask = torch.zeros((h, w), dtype=torch.uint8)
-    for id in range(n_labels):
-        save_mask[hard_pred[id][0]] = torch.tensor(id + 14, dtype=torch.uint8)
-        mask = hard_pred[id][0]
-        mask[hard_pred[id][0]] = torch.tensor(id + 14, dtype=torch.uint8)
-        mask = mask.cpu().numpy().astype(np.uint8)
-        save_colored_mask(mask, os.path.join('{}_{}_.png'.format(id, id)))
-
-    save_colored_mask(np.array(save_mask), os.path.join('final_{}.png'.format(id)))
-    # backmask = label_onehot.sum(dim=0) == 0
-    # label_onehot_save = torch.argmax(label_onehot, dim=0) + 14
-    # label_onehot_save[backmask] = 0
-    # save_colored_mask(np.array(label_onehot_save.cpu()), os.path.join('label_{}.png'.format(id)))
 
     scores = []
     for k in range(n_labels):
@@ -255,38 +229,12 @@ def inference_multi(model, image, label_onehot, support_images, support_labels_o
     soft_pred_onehot = torch.stack(soft_pred_onehot)
     hard_pred_1 = torch.tensor(soft_pred_onehot > 0.5, dtype=torch.uint8)
 
-    # save_mask = torch.zeros((1, 1, h, w), dtype=torch.uint8)
-    # for id in range(1, n_labels):
-    #     save_mask[0, 0, hard_pred_1[id][0] == 1] = torch.tensor(id + 26, dtype=torch.uint8)
-    #     mask = hard_pred_1[id][0]
-    #     mask[hard_pred_1[id][0]] = torch.tensor(id + 13, dtype=torch.uint8)
-    #     mask = mask.cpu().numpy().astype(np.uint8)
-    #     save_colored_mask(mask, os.path.join('uni{}_{}_.png'.format(id, id)))
-
-    # save_colored_mask(np.array(save_mask), os.path.join('/newdata3/xsa/ICUSeg/mambamodel/eval/spine_vis/final_uni{}_{}.png'.format(id, np.mean(scores))))
-    # backmask = label_onehot.sum(dim=0) == 0
-    # label_onehot_save = torch.argmax(label_onehot, dim=0) + 13
-    # label_onehot_save[backmask] = 0
-    # save_colored_mask(np.array(label_onehot_save.cpu()), os.path.join('/newdata3/xsa/ICUSeg/mambamodel/eval/spine_vis/label_uni{}_{}_{}.png'.format(id, str(name).split("/")[-1], np.mean(scores))))
     scores = []
     for k in range(1, n_labels):
         score = dice_score(hard_pred_1[k], label_onehot[k])
         scores.append(score)
         print(score)
     
-    # save_mask = torch.zeros((1, 1, h, w), dtype=torch.uint8)
-    # for id in range(1, n_labels):
-    #     save_mask[0, 0, hard_pred_1[id][0] == 1] = torch.tensor(id + 24, dtype=torch.uint8)
-
-    # save_mask = F.interpolate(save_mask, (448, 448), mode='nearest')
-    # save_mask = save_mask.squeeze(0).squeeze(0)
-    # save_colored_mask(np.array(save_mask), os.path.join('/newdata3/xsa/ICUSeg/mambamodel/eval/spine_vis/final_uni{}_{}.png'.format(np.mean(scores), str(name).split("/")[-1])))
-
-    # backmask = label_onehot[1:].sum(dim=0) == 0
-    # label_onehot_save = torch.argmax(label_onehot, dim=0) + 24
-    # label_onehot_save[backmask] = 0
-    # save_colored_mask(np.array(label_onehot_save.cpu()), os.path.join('/newdata3/xsa/ICUSeg/mambamodel/eval/spine_vis/label_uni{}_{}_{}.png'.format(id, np.mean(scores), str(name).split("/")[-1])))
-
     return {'Image': image,
             'Soft Prediction': soft_pred_onehot,
             'Prediction': hard_pred_1,
@@ -295,145 +243,68 @@ def inference_multi(model, image, label_onehot, support_images, support_labels_o
 
 
 
-# args = get_args_parser()
-# # model_univer = universeg(pretrained=True)
-# # _ = model_univer.to('cpu')
-# # model_univer.eval()
+args = get_args_parser()
+model_univer = universeg(pretrained=True)
+_ = model_univer.to('cpu')
+model_univer.eval()
 
-# opt = load_opt_from_config_files(args.conf_files)
-# opt = init_distributed(opt)
-# # # model_our = BaseModel(opt, build_model(opt)).cuda()
-# # # model_our.eval()
+opt = load_opt_from_config_files(args.conf_files)
+opt = init_distributed(opt)
 
-
-# # # model_dict = model_our.state_dict()
-# # # check_decoder = torch.load(os.path.join('/data1/paintercoco/output_dir_scripple_sammul/', 'checkpoint-22.pth'))
-# # # for k, v in check_decoder.items():
-# # #     if k in model_dict.keys():
-# # #         model_dict[k] = v
-
-# # # model_our.load_state_dict(model_dict)
-# # # model_our.model.backbone = PeftModel.from_pretrained(model_our.model.backbone, "/data1/paintercoco/output_dir_scripple_sammul/22/")
-# # # print("load success")
-
-# model_our_1 = MamICL(cfg=opt).cuda()
-# model_our_1.eval()
-
-# model_our_2 = MamICL(cfg=opt).cuda()
-# model_our_2.eval()
-
-# # # # model_our = torch.nn.parallel.DistributedDataParallel(model_our, device_ids=[args.local_rank],
-# # # #                                                           output_device=args.local_rank, find_unused_parameters=True)
-# # # # model_our = torch.nn.parallel.DistributedDataParallel(model_our, device_ids=[args.local_rank],
-# # # #                                                           output_device=args.local_rank, find_unused_parameters=True)
-# # # # 
-# model_dict = model_our_2.state_dict()
-# check_decoder = torch.load("/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_spsize448_onlydino/1400/checkpoint-1400_0.8553866446478365.pth")
-# for k, v in check_decoder['model'].items():
-#     if k in model_dict.keys():
-#         model_dict[k] = v
-# model_our_1.load_state_dict(model_dict)
-# model_our_1.backbone = PeftModel.from_pretrained(model_our_1.backbone, "/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_spsize448_onlydino/1400/")
-
-# model_our_2.load_state_dict(model_dict)
-# model_our_2.backbone = PeftModel.from_pretrained(model_our_2.backbone, "/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_224_ctm_sam_qkv_clsfusion_nochannel_wsam_1/7200/")
-# print("load success")
-# print("load success")
-
-# # model_our = MamICL(cfg=opt).cuda()
-# # model_our.eval()
-# # # # model_our = torch.nn.parallel.DistributedDataParallel(model_our, device_ids=[args.local_rank],
-# # #                                                         #   output_device=args.local_rank, find_unused_parameters=True)
-
-# # # # model_our = MamICL(cfg=opt).cuda()
-# # # # model_our.eval()
-# # model_dict = model_our.state_dict()
-# # # # check_decoder = torch.load("/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_224_ctm_sam_qkv_clsfusion_nochannel_wsam_1/checkpoint-24400_0.49497267878995616.pth")
-# # # # for k, v in check_decoder['model'].items():
-# # # #     if k in model_dict.keys():
-# # # #         model_dict[k] = v
-# # # # model_our.load_state_dict(model_dict)
-# # # # model_our.backbone = PeftModel.from_pretrained(model_our.backbone, "/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_224_ctm_sam_qkv_clsfusion_nochannel_wsam_1/")
-# # check_decoder = torch.load("/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_224_ctm_sam_qkv_clsfusion_nochannel_wsam_1/7200/checkpoint-7200_0.8408008830264327.pth")
-# # for k, v in check_decoder['model'].items():
-# #     if k in model_dict.keys():
-# #         model_dict[k] = v
-# # model_our.load_state_dict(model_dict)
-# # model_our.backbone = PeftModel.from_pretrained(model_our.backbone, "/newdata3/xsa/ICUSeg/mambamodel/output_dir_dino_224_ctm_sam_qkv_clsfusion_nochannel_wsam_1/7200/")
-
-# # print("load success")
+model_our_1 = MamICL(cfg=opt).cuda()
+model_our_1.eval()
 
 
-# repetation = 2
-# total_univer = []
-# total_ours = []
-# total_ours_1 = []
-# total_ours_2 = []
+model_dict = model_our_1.state_dict()
+check_decoder = torch.load("checkpoint_7200.pth")
+for k, v in check_decoder['model'].items():
+    if k in model_dict.keys():
+        model_dict[k] = v
+model_our_1.load_state_dict(model_dict)
+model_our_1.backbone = PeftModel.from_pretrained(model_our_1.backbone, "/newdata3/7200/")
 
-# # 8  1   16  1
-# d_support = SPINEDataset('JTSC', split='support', label=None, size=(args.input_size, args.input_size))
-# d_test = SPINEDataset('JTSC', split='test', label=None, size=(args.input_size, args.input_size))
-# cnt = 0
 
-# for rep in range(repetation):
-#     n_support = 64
-#     support_images, support_labels, name = zip(*itertools.islice(d_support, n_support))
-#     support_images = torch.stack(support_images).to('cpu')
-#     support_labels = torch.stack(support_labels).to('cpu')
+repetation = 200
+total_univer = []
+total_ours = []
+
+
+d_support = SPINEDataset('JTSC', split='support', label=None, size=(args.input_size, args.input_size))
+d_test = SPINEDataset('JTSC', split='test', label=None, size=(args.input_size, args.input_size))
+cnt = 0
+
+for rep in range(repetation):
+    n_support = 64
+    support_images, support_labels, name = zip(*itertools.islice(d_support, n_support))
+    support_images = torch.stack(support_images).to('cpu')
+    support_labels = torch.stack(support_labels).to('cpu')
     
-#     n_viz = 16
-#     n_predictions = 200
-#     results_univer = defaultdict(list)
-#     results_ours = defaultdict(list)
-#     results_ours_1 = defaultdict(list)
-#     results_ours_2 = defaultdict(list)
-#     results_persam = defaultdict(list)
+    n_viz = 16
+    n_predictions = 200
+    results_univer = defaultdict(list)
+    results_ours = defaultdict(list)
+    results_ours_1 = defaultdict(list)
+    results_ours_2 = defaultdict(list)
+    results_persam = defaultdict(list)
 
-#     idxs = np.random.permutation(len(d_test))[:n_predictions]
+    idxs = np.random.permutation(len(d_test))[:n_predictions]
 
-#     for i in tqdm(idxs):
-#         image, label, name = d_test[i]
+    for i in tqdm(idxs):
+        image, label, name = d_test[i]
 
-#         # vals_univer = inference_multi(model_univer, image, label, support_images, support_labels, name, 'cpu')
-#         vals_ours_1 = inference_multi_our(model_our_1, image, label, support_images, support_labels, 'cuda')
-#         vals_ours_2 = inference_multi_our(model_our_1, image, label, support_images, support_labels, 'cuda')
-
-#         # for k, v in vals_univer.items():
-#         #     results_univer[k].append(v)
-
-#         for k, v in vals_ours_1.items():
-#             results_ours_1[k].append(v)
+        vals_ours_1 = inference_multi_our(model_our_1, image, label, support_images, support_labels, 'cuda')
+    
+        for k, v in vals_ours_1.items():
+            results_ours_1[k].append(v)
         
-#         for k, v in vals_ours_2.items():
-#             results_ours_2[k].append(v)
+    scores_ours_1 = results_ours_1.pop('score')
 
+    avg_score_ours_1 = np.mean(scores_ours_1)
 
-#     scores_ours_1 = results_ours_1.pop('score')
-#     scores_ours_2 = results_ours_2.pop('score')
-#     # scores_univer = results_univer.pop('score')
+    total_ours_1.append(avg_score_ours_1)
 
-#     avg_score_ours_1 = np.mean(scores_ours_1)
-#     avg_score_ours_2 = np.mean(scores_ours_2)
-#     # avg_score_univer = np.mean(scores_univer)
+avg_ours_1 = np.mean(total_ours_1)
+std_ours_1 = np.std(total_ours_1)
 
-#     total_ours_1.append(avg_score_ours_1)
-#     total_ours_2.append(avg_score_ours_2)
-#     # total_univer.append(avg_score_univer)
-
-# avg_ours_1 = np.mean(total_ours_1)
-# std_ours_1 = np.std(total_ours_1)
-
-# avg_ours_2 = np.mean(total_ours_2)
-# std_ours_2 = np.std(total_ours_2)
-
-# # avg_univer = np.mean(total_univer)
-# # std_univer = np.std(total_univer)
-
-# # print('univer avg dice score after 5 repetations:{}'.format(avg_univer))
-# # print('univer std dice score after 5 repetations:{}'.format(std_univer))
-
-# print('Our11 avg dice score after 5 repetations:{}'.format(avg_ours_1))
-# print('Our std dice score after 5 repetations:{}'.format(std_ours_1))
-
-# print('Our22 avg dice score after 5 repetations:{}'.format(avg_ours_2))
-# print('Our std dice score after 5 repetations:{}'.format(std_ours_2))
+print('Our11 avg dice score after 5 repetations:{}'.format(avg_ours_1))
+print('Our std dice score after 5 repetations:{}'.format(std_ours_1))
